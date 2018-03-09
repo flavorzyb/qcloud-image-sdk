59c4b1ef7eca2e7d837bca3ab801ef77ed04a933
flavor <haker-haker@163.com>
2018-03-09 17:50:03 +0800

    update

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const FileUtil = require('./lib/file-util');
+const QCloudConfig = require('./lib/qcloud-config');
+const ImageClient = require('./lib/image-client');
+
+exports.FileUtil = FileUtil;
+exports.QCloudConfig = QCloudConfig;
+exports.ImageClient = ImageClient;
